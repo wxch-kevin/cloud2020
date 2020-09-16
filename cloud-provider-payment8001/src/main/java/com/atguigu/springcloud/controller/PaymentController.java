6fdcfb4a9b83811f9b50ed794b523b446e85bb46
kevin722 <kevin_wxch@163.com>
2020-09-16 17:45:21 +0800

    Initial commit

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,38 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Resource
+    private PaymentService paymentService;
+
+    @PostMapping(value = "/payment/create")
+    public CommonResult create(Payment payment){
+        int result = paymentService.create(payment);
+        log.info("-----插入:"+result);
+        if (result>0){
+            return new CommonResult(200,"插入成功",result);
+        }else {
+            return new CommonResult(444,"插入失败",null);
+        }
+    }
+
+    @GetMapping(value = "/payment/get/{id}")
+    public CommonResult getPaymentById(@PathVariable("id") int id){
+        Payment payment = paymentService.getPaymentById(id);
+        if (payment != null){
+            return new CommonResult(200,"success",payment);
+        }else {
+            return new CommonResult(444,"failed",null);
+        }
+    }
+}
